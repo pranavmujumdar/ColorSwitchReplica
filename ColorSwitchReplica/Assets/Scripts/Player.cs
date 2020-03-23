9397248604cdbbd0e46b13cb4296588bfd70512d
Pranav Mujumdar <psmujumdar@gmail.com>
2020-03-23 11:46:56 -0400

    Created Unity 2d game, the player, the colorwheel

---
+++ ColorSwitchReplica/Assets/Scripts/Player.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    public float jumpForce = 10f;
+
+    public Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Jump") || Input.GetMouseButtonDown(0))
+        {
+            rb.velocity = Vector2.up * jumpForce;
+        }
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log(collision.tag);
+    }
+}
---
+++ ColorSwitchReplica/Assets/Scripts/Rotator.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    public float rotateSpeed = 100f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0f, 0f, rotateSpeed * Time.deltaTime);
+    }
+}
