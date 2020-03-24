2f0f2ad565211fe07d31898f2131753a4d70fb24
Pranav Mujumdar <psmujumdar@gmail.com>
2020-03-23 22:28:25 -0400

    Added ScoreTracker

---
+++ ColorSwitchReplica/Assets/Scripts/ScoreTracker.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using TMPro;
+public class ScoreTracker : MonoBehaviour
+{
+    public TextMeshProUGUI scoreText;
+    private float score = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetScore();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.y > score)
+        {
+            score = transform.position.y;
+            SetScore();
+        }
+       
+    }
+
+    void SetScore()
+    {
+        scoreText.text = score.ToString("0");
+    }
+}
